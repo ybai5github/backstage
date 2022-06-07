19d0260120a9b1160be3c9aabf3e4c930fda903c
Gustaf Räntilä <g.rantila@gmail.com>
2022-06-07 13:50:18 +0200

    fix(extendable-components): Make link component ref versioned
    Signed-off-by: Gustaf Räntilä <g.rantila@gmail.com>

--- packages/core-components/src/components/Link/Link.tsx
+++ packages/core-components/src/components/Link/Link.tsx
@@ -91,7 +91,7 @@ export const {
    */
   Component: Link,
 } = createExtendableComponentForwardRef<LinkProps, LinkContext>({
-  id: 'Link',
+  id: 'Link:v1',
   Provider: ({ props, Component }) => {
     const to = String(props.to);
     const value: LinkContext = { to, children: props.children };
