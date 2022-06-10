b8d0b3065a2dcc3fdc30cad8198da1f3a2b0f9c7
Gustaf Räntilä <g.rantila@gmail.com>
2022-06-10 17:24:01 +0200

    fix(link): Fixed link 'id' which must be lowercase
    Signed-off-by: Gustaf Räntilä <g.rantila@gmail.com>

--- packages/core-components/src/components/Link/Link.tsx
+++ packages/core-components/src/components/Link/Link.tsx
@@ -91,7 +91,7 @@ export const {
    */
   Component: Link,
 } = createAdaptableForwardableComponent<LinkProps, LinkContext>({
-  id: 'Link:v1',
+  id: 'link:v1',
   Provider: ({ props, Component }) => {
     const to = String(props.to);
     const value: LinkContext = { to, children: props.children };
